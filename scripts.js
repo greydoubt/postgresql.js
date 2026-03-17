ed4cff19117c610d72f323c2292f925c708ec5bf
λx . ophidia ereц grex          .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-03-17 15:40:29 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
+import schema from "./schema.json"
+import table from "./types.json"
+import * from "./workers.js"
 
+import { validateSchema } from "./types"
+
+const result = validateSchema(schema, table)
+
+console.log(result)
